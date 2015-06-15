7b3d3bf59a1407dfb7728dc2e0e939affc1c96a5
Masa <masa@bespoke.is>
2015-06-15 15:24:22 -0700

    all pyc files removed

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,18 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User 
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
+from rest_framework import permissions
+
+
+class UserList(generics.ListAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
   """
@@ -8,10 +20,22 @@ class SnippetList(generics.ListCreateAPIView):
   """
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer 
-  
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   """
   Retrieve, update or delete a code snippet.
   """
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
+
+
+def perform_create(self, serializer):
+  """
+  perform_create() method on our snippet views, that allows us to modify 
+  how the instance save is managed, and handle any information that is 
+  implicit in the incoming request or requested URL.
+  """
+  serializer.save(owner=self.request.user)
+

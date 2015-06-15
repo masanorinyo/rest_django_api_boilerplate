f31c1642058c44543fd53c3f6d6a35521ef819d8
Masa <masa@bespoke.is>
2015-06-15 12:13:15 -0700

    Serializer added

--- rest_api_example/settings.py
+++ rest_api_example/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "snippets"
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'rest_api_example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'example_api',
+        "USER" : "postgres",
+        # "PASSWORD" : "testtest",
+        "PORT" : "5432"
     }
 }
 

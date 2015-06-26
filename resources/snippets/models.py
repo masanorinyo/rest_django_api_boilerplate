00cd541ec357ae78dc86234ea3d2b146d462ab97
Masa <masa@bespoke.is>
2015-06-26 16:00:05 -0700

    changing the name of snippet related field

--- resources/snippets/models.py
+++ resources/snippets/models.py
@@ -18,7 +18,7 @@ class Snippet(models.Model):
   linenos = models.BooleanField(default=False)
   language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
   style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
-  owner = models.ForeignKey('auth.User', related_name='users')
+  user = models.ForeignKey('auth.User', related_name='users')
   highlighted = models.TextField()
 
   class Meta:

3736c267a3a6ba5ab01e6acf1acc7f5822ae1992
Masa <masa@bespoke.is>
2015-07-06 12:41:12 -0700

    fixing the issue of not being able to make "POST" request

--- api/v2/snippets/views.py
+++ api/v2/snippets/views.py
@@ -9,3 +9,6 @@ from rest_framework import viewsets
 class SnippetViewSet(viewsets.ModelViewSet):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    def perform_create(self, serializer):
+      serializer.save(user=self.request.user) 

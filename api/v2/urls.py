e502f430e7910fc8314870ba56be69f8e16ea634
Masa <masa@bespoke.is>
2015-07-06 19:31:44 -0700

    extracting url into api app folder

--- api/v2/urls.py
+++ api/v2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import *
-from users.views import UserViewSet
 from snippets.views import SnippetViewSet
+from users.views import UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -9,10 +9,7 @@ router.register(r'snippets', SnippetViewSet)
 
 urlpatterns = router.urls
 
-
-# [TODO] - needs to move these to user api directory
-urlpatterns += [
-  url(r'^users/(?P<pk>[0-9]+)/relationships/$', UserViewSet.as_view({'get':'relationships'}), name="relationships"),
-  url(r'^users/(?P<pk>[0-9]+)/relationships/snippets/$', UserViewSet.as_view({'get':'snippet_relationships'}), name="snippet_relationships"),
-  url(r'^users/(?P<pk>[0-9]+)/snippets/$', UserViewSet.as_view({'get':'snippet_included'}), name="snippet_included"),
-]
+urlpatterns += patterns('',
+  # default version needs to be always the most recent API urls 
+  url(r'', include('api.v2.users.urls')),
+)
